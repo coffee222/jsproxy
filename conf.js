a4c01bd5f332aa1d9a1257a2ab65a3c1692a5978
bestgu <38128631+bestgu@users.noreply.github.com>
2019-10-07 09:50:27 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,7 +29,7 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        'https://bestgu.github.io/proxy': 1,
+        'https://proxy.beigu.workers.dev': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
